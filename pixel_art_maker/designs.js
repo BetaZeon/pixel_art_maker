db2ac87f8a9e87d5e331bb860365b27c7e50f9bf
Ashwani Tripathi <ashwanitripathi144@gmail.com>
2020-06-01 01:06:40 +0530

    Update designs.js

--- pixel_art_maker/designs.js
+++ pixel_art_maker/designs.js
@@ -1,39 +1,36 @@
-const $tableElement = $('#pixelCanvas');
-const $inputh = $('#inputh');  // Storing grid h value
-const $inputw = $('#inputw');  // Storing grid w value
-const $colorPicker = $('#colorPicker');  //Storing color value
-// this sets the size of grid
-$('#pickingSize').submit( event => {
+
+const $colorPicker = $('#colorPicker');
+// Select size input
+var h, w, p;
+// When size is submitted by the user, call createGrid()
+$("#sizePicker").submit(function (event) {
     event.preventDefault();
-
-    let w = $inputw.val();
-    let h = $inputh.val();
-
-    //$tableElement.html('');
-
+    h = $("#inputHeight").val();
+    w = $("#inputWidth").val();
     createGrid(h, w);
-    cellClickListener();
+    addCellClickListener();
+
 });
-//Function to change color of pixel when clicked.
-function cellClickListener() {
-    // When the cell is clicked, the background color changes to the selected color
+
+// Creating table rows and table data
+function createGrid(h, w) {
+// remove current grid if one exists
+    $("tr").remove();
+for (var i = 1; i <= h; i++) {
+       $("#pixelCanvas").append("<tr></tr>");
+           for (var j = 0; j < w; j++) {
+             $('tr:last-child').append('<td></td>');
+             $('td').attr("class", 'color');
+           }
+    }
+}
+// Function to Paint td with color and also remove color
+// Adding and removing color to table data
+// When table data is clicked by a user, add background color or revert change to table data
+    
+function addCellClickListener() {
     $('td').click( event => {
         let color = $colorPicker.val();
         $(event.currentTarget).css("background-color", color)
     });
 };
-
-function createGrid(h, w) {
-    //Loop to Insert row
-    for(let x = 0; x < h; x++) {
-        $tableElement.append('<tr></tr>');
-    };
-
-    //Loop to Insert Column
-
-    for(let x = 0; x < w; x++) {
-        $('tr').append('<td></td>');
-    };
-};
-
-createGrid(10, 10);   //This creates a 10x10 grid by default.
